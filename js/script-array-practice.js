9a47d6c83354d1382c7e5a4ba56f8e1eb9951786
Alan Chen <alrichen@ucsc.edu>
2025-04-15 01:57:11 -0700

    Working on part 1

---
+++ js/script-array-practice.js
@@ -0,0 +1,159 @@
+//STEP 1
+// const favoriteMovies = ["Wall-E", "Avatar", "Suzume", "Top Gun: Maverick", "Avatar: The Way of Water" ]
+// console.log(favoriteMovies[1])
+//STEP 2
+// let favoriteMovies = new Array(5)
+// favoriteMovies[0] = "Wall-E"
+// favoriteMovies[1] = "Avatar"
+// favoriteMovies[2] = "Suzume"
+// favoriteMovies[3] = "Top Gun: Maverick"
+// favoriteMovies[4] = "Avatar: The Way of Water"
+// console.log(favoriteMovies[0])
+//STEP 3
+// let favoriteMovies = new Array(5)
+// favoriteMovies[0] = "Wall-E"
+// favoriteMovies[1] = "Avatar"
+// favoriteMovies[2] = "Suzume"
+// favoriteMovies[3] = "Top Gun: Maverick"
+// favoriteMovies[4] = "Avatar: The Way of Water"
+// console.log(favoriteMovies[0])
+// favoriteMovies.splice(2, 0, "John Wick")
+// console.log(favoriteMovies.length)
+//STEP 4
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// delete favoriteMovies[0]
+// console.log(favoriteMovies)
+//STEP 5
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+// for (const index in favoriteMovies) {
+//     console.log(favoriteMovies[index])
+// }
+//STEP 6
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+// for (const movie of favoriteMovies) {
+//     console.log(movie)
+// }
+//STEP 7
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+// favoriteMovies.sort()
+// for (const movie of favoriteMovies) {
+//     console.log(movie)
+// }
+//STEP 8
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+
+// let leastFavMovies = []
+// leastFavMovies.push("2001: A Space Odyssey")
+// leastFavMovies.push("Deadpool & Wolverine")
+// leastFavMovies.push("Captain Marvel")
+// console.log("\n")
+// console.log("Movies I like:")
+// console.log("\n")
+// for (const index in favoriteMovies) {
+//     console.log(favoriteMovies[index])
+// }
+// console.log("\n")
+// console.log("Movies I regret watching:")
+// console.log("\n")
+// for (const movie of leastFavMovies){
+//     console.log(movie)
+// }
+// console.log("\n")
+//STEP 9
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+
+// let leastFavMovies = []
+// leastFavMovies.push("2001: A Space Odyssey")
+// leastFavMovies.push("Deadpool & Wolverine")
+// leastFavMovies.push("Captain Marvel")
+// let movies = favoriteMovies.concat(leastFavMovies)
+// movies.sort()
+// movies.reverse()
+// console.log(movies)
+//STEP 10
+// let favoriteMovies = []
+// favoriteMovies.push("Wall-E")
+// favoriteMovies.push("Avatar")
+// favoriteMovies.push("Suzume")
+// favoriteMovies.push("Top Gun: Maverick")
+// favoriteMovies.push("Avatar: The Way of Water")
+// favoriteMovies.push("Kiki's Delivery Service")
+// favoriteMovies.push("Robocop")
+
+// let leastFavMovies = []
+// leastFavMovies.push("2001: A Space Odyssey")
+// leastFavMovies.push("Deadpool & Wolverine")
+// leastFavMovies.push("Captain Marvel")
+// let movies = favoriteMovies.concat(leastFavMovies)
+// movies.sort()
+// movies.reverse()
+// console.log(movies.at(length-1))
+//STEP 11
+let favoriteMovies = []
+favoriteMovies.push("Wall-E")
+favoriteMovies.push("Avatar")
+favoriteMovies.push("Suzume")
+favoriteMovies.push("Top Gun: Maverick")
+favoriteMovies.push("Avatar: The Way of Water")
+favoriteMovies.push("Kiki's Delivery Service")
+favoriteMovies.push("Robocop")
+
+let leastFavMovies = []
+leastFavMovies.push("2001: A Space Odyssey")
+leastFavMovies.push("Deadpool & Wolverine")
+leastFavMovies.push("Captain Marvel")
+let movies = favoriteMovies.concat(leastFavMovies)
+movies.sort()
+movies.reverse()
+console.log(movies.at(0))
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17

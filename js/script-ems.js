1930805d6965431291198a73ea8b5a2d573c6bdf
Alan Chen <alrichen@ucsc.edu>
2025-04-16 01:00:04 -0700

    added employee array as function parameter

--- js/script-ems.js
+++ js/script-ems.js
@@ -1,5 +1,5 @@
 // CREATE AN ARRAY OF EMPLOYEES
-employees = [
+let employees = [
     // ID#, name str, 4 digit#, email str, department str
     [21324114, "Mary Jane", 3773, "maryja@bebcorp.com", "Administrative"],
     [32489432, "Dan Daniels", 3642, "danda@bebcorp.com", "Engineering"],
@@ -24,41 +24,47 @@ buildGrid()
 // ADD EMPLOYEE
 let form = $("addForm") 
 form.addEventListener('submit', (e) => {
-    // PREVENT FORM SUBMISSION
-    e.preventDefault()
-    // GET THE VALUES FROM THE TEXT BOXES
-    let id = $('id').value;
-    let name = $('name').value;
-    let extension = $('extension').value;
-    let department = $('department').value;
-    let email = $('email').value;
-    // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
-    let newEmployee = [id, name, extension, email, department]
-    // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
-    employees.push(newEmployee)
-    // BUILD THE GRID
-    buildGrid()
-    // RESET THE FORM
-    form.reset()
-    // SET FOCUS BACK TO THE ID TEXT BOX
-    $('id').focus()
+    addEmployee(e, employees)
+    function addEmployee(e, employees) {
+        // PREVENT FORM SUBMISSION
+        e.preventDefault()
+        // GET THE VALUES FROM THE TEXT BOXES
+        let id = $('id').value;
+        let name = $('name').value;
+        let extension = $('extension').value;
+        let department = $('department').value;
+        let email = $('email').value;
+        // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
+        let newEmployee = [id, name, extension, email, department]
+        // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+        employees.push(newEmployee)
+        // BUILD THE GRID
+        buildGrid()
+        // RESET THE FORM
+        form.reset()
+        // SET FOCUS BACK TO THE ID TEXT BOX
+        $('id').focus()
+    }
 });
 
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
-    if (e.target.tagName === "BUTTON") {
-        // CONFIRM THE DELETE
-        if (confirm("Are you sure you want to delete this employee?")) {
-            // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE)
-            let row = e.target.parentNode
-            // console.log(row)  // Current row of click event
-            // console.log(row.parentNode) // The entire table
-            
-            // Row index starts at 1 in HTML
-            let index = row.rowIndex - 1        
-            employees.splice(index, 1)
-            // BUILD THE GRID 
-            buildGrid()
+    deleteEmployee(e,employees)
+    function deleteEmployee(e, employees) {
+        if (e.target.tagName === "BUTTON") {
+            // CONFIRM THE DELETE
+            if (confirm("Are you sure you want to delete this employee?")) {
+                // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE)
+                let row = e.target.parentNode
+                // console.log(row)  // Current row of click event
+                // console.log(row.parentNode) // The entire table
+                
+                // Row index starts at 1 in HTML
+                let index = row.rowIndex - 1        
+                employees.splice(index, 1)
+                // BUILD THE GRID 
+                buildGrid()
+            }
         }
     }
 });
